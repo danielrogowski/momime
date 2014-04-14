503dfce3e0d022920e778d1c66796490e4dfb810
Nigel Gay <nigelgay@hotmail.ca>
2014-04-14 22:50:36 +0000

    Missed from previous commit due to rename

---
+++ Client/src/main/java/momime/client/graphics/database/GraphicsDatabaseFactory.java
@@ -0,0 +1,22 @@
+package momime.client.graphics.database;
+
+/**
+ * Factory interface for creating graphics database related objects from prototypes defined in the spring XML
+ */
+public interface GraphicsDatabaseFactory
+{
+	/**
+	 * @return Newly created smoothed tile type
+	 */
+	public SmoothedTileTypeEx createSmoothedTileType ();
+	
+	/**
+	 * @return Newly created tile set
+	 */
+	public TileSetEx createTileSet ();
+	
+	/**
+	 * @return Newly created animation
+	 */
+	public AnimationEx createAnimation ();
+}

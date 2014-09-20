0cb15f45d2d0ae76567cc2e93c9eaa05ac1a44d2
Nigel Gay <nigelgay@hotmail.ca>
2014-09-20 15:07:53 +0000

    Fixes to make editors work correctly in assembled zip (relative paths handled better; creates backups folder if it doesn't exist; supply logging file for editors; use friendlier looking .bat names)

--- Assembly/src/main/deploy/MoMIMEEditors-Client-Graphics.bat
+++
@@ -1,2 +0,0 @@
-for /F %%a in (lib\classpaths\editors-classpath.txt) do set libcp=%%a
-java -classpath lib\momime-editors-${project.version}.jar;%libcp% momime.editors.client.graphics.GraphicsEditor
---
+++ Assembly/src/main/deploy/editors/editClientLanguageXmls.bat
@@ -0,0 +1,2 @@
+for /F %%a in (..\lib\classpaths\editors-classpath.txt) do set libcp=%%a
+java -classpath ..\lib\momime-editors-${project.version}.jar;%libcp% momime.editors.client.language.LanguageEditor

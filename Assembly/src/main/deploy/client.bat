e3371fe6bb29c83ebfd0287189deff34c54eae27
Nigel Gay <nigelgay@hotmail.ca>
2016-03-16 19:33:15 +0000

    Set window titles when running batch files

--- Assembly/src/main/deploy/client.bat
+++ Assembly/src/main/deploy/client.bat
@@ -1,2 +1,3 @@
+title MoM IME Client ${momime.client.version}
 for /F %%a in (lib\classpaths\client-classpath-windows.txt) do set libcp=%%a
 java -Xmx1024m -classpath lib\momime-client-${momime.client.version}.jar;%libcp% momime.client.MomClientKickOff client
--- Assembly/src/main/deploy/server.bat
+++ Assembly/src/main/deploy/server.bat
@@ -1,2 +1,3 @@
+title MoM IME Server ${momime.server.version}
 for /F %%a in (lib\classpaths\server-classpath-windows.txt) do set libcp=%%a
 java -Xmx1024m -classpath lib\momime-server-${momime.server.version}.jar;%libcp% momime.server.MomServerKickOff server

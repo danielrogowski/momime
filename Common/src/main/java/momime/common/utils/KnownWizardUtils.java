45ee4882aab96e6ccd0c862f9d27d9e4f19da357
Nigel Gay <nigelgay@hotmail.ca>
2021-12-25 08:10:02 -0330

    Meeting wizards part 1 - Data model changes, and utils method for finding wizard details

---
+++ Common/src/main/java/momime/common/utils/KnownWizardUtils.java
@@ -0,0 +1,24 @@
+package momime.common.utils;
+
+import java.util.List;
+
+import momime.common.database.RecordNotFoundException;
+import momime.common.messages.KnownWizardDetails;
+
+/**
+ * Methods for finding KnownWizardDetails from the list.
+ * When used on client, the list will be obtained from getClient ().getOurPersistentPlayerPrivateKnowledge ().getKnownWizardDetails ().
+ * WHen used on server, the list will be obtained from gsk.getTrueWizardDetails ().
+ */
+public interface KnownWizardUtils
+{
+	/**
+	 * @param knownWizardDetailsList List of KnownWizardDetails to search
+	 * @param playerID playerID to search for
+	 * @param caller Name of method calling this, for inclusion in debug message if there is a problem (the server should always send the client details about a wizard before the client may need it)
+	 * @return Info about the wizard that the requested player is playing as
+	 * @throws RecordNotFoundException If the wizard isn't found in the list
+	 */
+	public KnownWizardDetails findKnownWizardDetails (final List<KnownWizardDetails> knownWizardDetailsList, final int playerID, final String caller)
+		throws RecordNotFoundException;
+}
---
+++ Common/src/test/java/momime/common/utils/TestKnownWizardUtilsImpl.java
@@ -0,0 +1,70 @@
+package momime.common.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import momime.common.database.RecordNotFoundException;
+import momime.common.messages.KnownWizardDetails;
+
+/**
+ * Tests the KnownWizardUtilsImpl class
+ */
+@ExtendWith(MockitoExtension.class)
+public final class TestKnownWizardUtilsImpl
+{
+	/**
+	 * Tests the findKnownWizardDetails method
+	 * @throws Exception If there is a problem
+	 */
+	@Test
+	public final void testFindKnownWizardDetails_Exists () throws Exception
+	{
+		// Build sample list
+		final List<KnownWizardDetails> list = new ArrayList<KnownWizardDetails> ();
+		for (int n = 1; n <= 3; n++)
+		{
+			final KnownWizardDetails details = new KnownWizardDetails ();
+			details.setPlayerID (n);
+			list.add (details);
+		}
+		
+		// Set up object to test
+		final KnownWizardUtilsImpl utils = new KnownWizardUtilsImpl ();
+		
+		// Run method
+		assertEquals (2, utils.findKnownWizardDetails (list, 2, "Unit test").getPlayerID ());
+	}
+
+	/**
+	 * Tests the findKnownWizardDetails method
+	 * @throws Exception If there is a problem
+	 */
+	@Test
+	public final void testFindKnownWizardDetails_NotExists () throws Exception
+	{
+		// Build sample list
+		final List<KnownWizardDetails> list = new ArrayList<KnownWizardDetails> ();
+		for (int n = 1; n <= 3; n++)
+		{
+			final KnownWizardDetails details = new KnownWizardDetails ();
+			details.setPlayerID (n);
+			list.add (details);
+		}
+		
+		// Set up object to test
+		final KnownWizardUtilsImpl utils = new KnownWizardUtilsImpl ();
+		
+		// Run method
+		assertThrows (RecordNotFoundException.class, () ->
+		{
+			utils.findKnownWizardDetails (list, 4, "Unit test");
+		});
+	}
+}

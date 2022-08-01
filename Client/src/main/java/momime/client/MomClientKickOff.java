1ea905a87c1feeab5d8b89ecc7565e351bd251dc
Nigel Gay <nigelgay@hotmail.ca>
2022-08-01 07:15:32 -0230

    Remove JDK version check; now ProcessUtils isn't even needed in NdgUtils

--- Client/src/main/java/momime/client/MomClientKickOff.java
+++ Client/src/main/java/momime/client/MomClientKickOff.java
@@ -2,10 +2,6 @@ package momime.client;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.ndg.utils.ProcessUtilsImpl;
-
-import momime.common.MomCommonConstants;
-
 /**
  * The MomClientImpl class declares a log, so as soon as the classloader touches it, it fires up log4j.
  * Therefore we have to put the 'main' method in a separate class that doesn't declare a log, so that
@@ -24,9 +20,6 @@ public final class MomClientKickOff
 			// Allow reading config from the 'client' folder after the app is assembled, but the root folder when in Eclipse
 			final String configDir = (args.length == 0) ? "" : (args [0] + "/");
 			System.setProperty ("configDir", configDir);
-			
-			// Ensure minimum required JVM version
-			new ProcessUtilsImpl ().ensureMinimumJavaVersion ("MoM IME Client", MomCommonConstants.JAVA_REQUIRED_VERSION);
 			
 			// Initialize logging first, in case debug logging for spring itself is enabled
 			System.setProperty ("log4j.configurationFile", "" + configDir + "MoMIMEClientLogging.xml");

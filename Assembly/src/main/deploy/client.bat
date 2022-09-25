c148dcb22ce62b4582f9d587e4de8df57bba978c
Nigel Gay <nigelgay@hotmail.ca>
2022-09-25 17:35:18 -0230

    Pre-create folders to install mod files into

--- Assembly/src/main/deploy/client.bat
+++ Assembly/src/main/deploy/client.bat
@@ -5,4 +5,4 @@ if exist jre\bin\java.exe set javapath=jre\bin\
 
 for /F %%a in (lib\classpaths\client-classpath-windows.txt) do set libcp=%%a
 %javapath%java -version
-%javapath%java -Xmx2048m -classpath lib\momime-client-${project.version}.jar;%libcp% momime.client.MomClientKickOff client
+%javapath%java -Xmx2048m -classpath lib\momime-client-${project.version}.jar;%libcp%;client\mods\* momime.client.MomClientKickOff client

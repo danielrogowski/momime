95a0af9ff924b2d61e8685d3bedbeb5a6b8b9bae
Nigel Gay <nigelgay@hotmail.ca>
2022-12-23 20:08:52 -0330

    New page turn animation supports turning multiple pages at the same time

--- Client/src/main/java/momime/client/ui/frames/SpellBookNewUI.java
+++ Client/src/main/java/momime/client/ui/frames/SpellBookNewUI.java
@@ -8,11 +8,14 @@ import java.awt.GridBagLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -30,17 +33,14 @@ public final class SpellBookNewUI extends MomClientFrameUI
 	/** Class logger */
 	private final static Log log = LogFactory.getLog (SpellBookNewUI.class);
 
-	/** How much higher than the book itself the page turn anim is drawn */
-	private final static int ANIM_VERTICAL_OFFSET = 9;
-
-	/** How many pixels were added at the top of the old background.png compared to the new cover.png */
-	private final static int OLD_BACKGROUND_PADDING_TOP = 7;
+	/** How much space to leave above the cover background */
+	private final static int BACKGROUND_PADDING_TOP = 83;
 	
-	/** How many pixels were added at the bottom of the old background.png compared to the new cover.png */
-	private final static int OLD_BACKGROUND_PADDING_BOTTOM = 14;
+	/** How much space to leave below the cover background */
+	private final static int BACKGROUND_PADDING_BOTTOM = 30;
 	
 	/** Y coordinate of first (lowest) pages */
-	private final static int FIRST_PAGE_TOP = 40;
+	private final static int FIRST_PAGE_BOTTOM = (BACKGROUND_PADDING_TOP * 2) + 322;
 	
 	/** X coordinate of first (lowest) left page */
 	private final static int FIRST_LEFT_PAGE = 10;
@@ -57,20 +57,29 @@ public final class SpellBookNewUI extends MomClientFrameUI
 	/** There's always 1 page on the left and 1 page on the right.  Besides those, this many pages need to be stacked on top, on one side, the other, or split between the two */
 	private final static int FLIPPABLE_PAGE_COUNT = 12;
 	
-	/** Image of left page */
-	private BufferedImage pageLeft;
+	/** Old one with only 2 frames was ran at 5 FPS */
+	private final static int PAGE_FLIP_ANIMATION_SPEED = 12;
+	
+	/** How many frames apart the animations for adjacent pages must be kept */
+	private final static int ANIMATION_MINIMUM_SEPARATION = 3;
 
-	/** Image of right page */
-	private BufferedImage pageRight;
+	/** Images of left pages at various stages of turning */
+	private List<BufferedImage> pageLeftFrames = new ArrayList<BufferedImage> ();
+
+	/** Images of right pages at various stages of turning*/
+	private List<BufferedImage> pageRightFrames = new ArrayList<BufferedImage> ();
 	
 	/** Content pane */
 	private JPanel contentPane;
 	
-	/** Currently visible left page */
-	private int leftPageNumber = 0;
+	/** 0..5 = progress through 6 images in pageLeftFrames, 6..11 = progress through 6 images in pageRightFrames.  There are FLIPPABLE_PAGE_COUNT+2 items in this list. */
+	private List<Integer> pageState = new ArrayList<Integer> ();
 	
-	/** Currently visible right page (normally this is leftPageNumber+1, but they're updated during different frames of page turn animations) */
-	private int rightPageNumber = 1;
+	/** Number of frames of animation that exist for each page, so valid page states are 0 .. pageStateCount - 1 */
+	private int pageStateCount;
+	
+	/** Number of pages we want fully on the left, with all the others fully on the right.  Animation timer will attempt to update all the pages towards this state. */
+	private int desiredPagesOnLeft;
 	
 	/** Turn page left action */
 	private Action turnPageLeftAction;
@@ -87,52 +96,50 @@ public final class SpellBookNewUI extends MomClientFrameUI
 	{
 		// Load images
 		final BufferedImage cover = getUtils ().loadImage ("/momime.client.graphics/ui/spellBook/cover.png");
-		pageLeft = getUtils ().loadImage ("/momime.client.graphics/ui/spellBook/page-left-1.png");
-		pageRight = getUtils ().loadImage ("/momime.client.graphics/ui/spellBook/page-right-1.png");
+		pageLeftFrames.add (getUtils ().loadImage ("/momime.client.graphics/ui/spellBook/page-left-1.png"));
+		pageRightFrames.add (getUtils ().loadImage ("/momime.client.graphics/ui/spellBook/page-right-1.png"));
 		final BufferedImage pageLeftCorner = getUtils ().loadImage ("/momime.client.graphics/ui/spellBook/page-left-corner.png");
 		final BufferedImage pageRightCorner = getUtils ().loadImage ("/momime.client.graphics/ui/spellBook/page-right-corner.png");
 		
-		if ((pageLeft.getWidth () != pageRight.getWidth ()) || (pageLeft.getHeight () != pageRight.getHeight ()))
+		if ((pageLeftFrames.get (0).getWidth () != pageRightFrames.get (0).getWidth ()) || (pageLeftFrames.get (0).getHeight () != pageRightFrames.get (0).getHeight ()))
 			throw new IOException ("Left and right page images are different sizes");
 
-		if ((pageLeft.getWidth () != pageLeftCorner.getWidth ()) || (pageLeft.getHeight () != pageLeftCorner.getHeight ()))
+		if ((pageLeftFrames.get (0).getWidth () != pageLeftCorner.getWidth ()) || (pageLeftFrames.get (0).getHeight () != pageLeftCorner.getHeight ()))
 			throw new IOException ("Left page and with corner images are different sizes");
 		
-		if ((pageRight.getWidth () != pageRightCorner.getWidth ()) || (pageRight.getHeight () != pageRightCorner.getHeight ()))
+		if ((pageRightFrames.get (0).getWidth () != pageRightCorner.getWidth ()) || (pageRightFrames.get (0).getHeight () != pageRightCorner.getHeight ()))
 			throw new IOException ("Right page and with corner images are different sizes");
 		
 		final Dimension fixedSize = new Dimension (cover.getWidth () * 2,
-			(cover.getHeight () + ANIM_VERTICAL_OFFSET + OLD_BACKGROUND_PADDING_TOP + OLD_BACKGROUND_PADDING_BOTTOM) * 2);
+			(cover.getHeight () + BACKGROUND_PADDING_TOP + BACKGROUND_PADDING_BOTTOM) * 2);
 		
 		// Generate animation frames
-		generatePageTurnAnimationFrame (pageRight, 77, 47, 106, 243, false, null);
-		generatePageTurnAnimationFrame (pageRight, 80, 60, 115, 220, false, null);
-		generatePageTurnAnimationFrame (pageRight, 83, 73, 126, 180, false, null);
-		generatePageTurnAnimationFrame (pageRight, 86, 86, 140, 0, false, null);
-		generatePageTurnAnimationFrame (pageRight, 140, 150, 70, 0, false, null);
+		pageRightFrames.add (generatePageTurnAnimationFrame (pageRightFrames.get (0), 77, 47, 106, 243, false, null));
+		pageRightFrames.add (generatePageTurnAnimationFrame (pageRightFrames.get (0), 80, 60, 115, 220, false, null));
+		pageRightFrames.add (generatePageTurnAnimationFrame (pageRightFrames.get (0), 83, 73, 126, 180, false, null));
+		pageRightFrames.add (generatePageTurnAnimationFrame (pageRightFrames.get (0), 86, 86, 140, 0, false, null));
+		pageRightFrames.add (generatePageTurnAnimationFrame (pageRightFrames.get (0), 140, 150, 70, 0, false, null));
 
-		generatePageTurnAnimationFrame (pageLeft, 77, 47, 106, 243, true, null);
-		generatePageTurnAnimationFrame (pageLeft, 80, 60, 115, 220, true, null);
-		generatePageTurnAnimationFrame (pageLeft, 83, 73, 126, 180, true, null);
-		generatePageTurnAnimationFrame (pageLeft, 86, 86, 140, 0, true, null);
-		generatePageTurnAnimationFrame (pageLeft, 140, 150, 70, 0, true, null);
+		pageLeftFrames.add (generatePageTurnAnimationFrame (pageLeftFrames.get (0), 77, 47, 106, 243, true, null));
+		pageLeftFrames.add (generatePageTurnAnimationFrame (pageLeftFrames.get (0), 80, 60, 115, 220, true, null));
+		pageLeftFrames.add (generatePageTurnAnimationFrame (pageLeftFrames.get (0), 83, 73, 126, 180, true, null));
+		pageLeftFrames.add (generatePageTurnAnimationFrame (pageLeftFrames.get (0), 86, 86, 140, 0, true, null));
+		pageLeftFrames.add (generatePageTurnAnimationFrame (pageLeftFrames.get (0), 140, 150, 70, 0, true, null));
+		
+		// Populate initial page state
+		// 1st page is fully left, all the rest are fully right
+		pageStateCount = pageLeftFrames.size () + pageRightFrames.size ();
+		pageState.add (0);
+		for (int n = 0; n <= FLIPPABLE_PAGE_COUNT; n++)
+			pageState.add (pageStateCount - 1);
+		
+		desiredPagesOnLeft = 1;
 		
 		// Actions
 		final Action closeAction = new LoggingAction ("X", (ev) -> getFrame ().setVisible (false));
 		
-		turnPageLeftAction = new LoggingAction ("<", (ev) ->
-		{
-			leftPageNumber = leftPageNumber - 2;
-			rightPageNumber = rightPageNumber - 2;
-			contentPane.repaint ();
-		});
-		
-		turnPageRightAction = new LoggingAction (">", (ev) ->
-		{
-			leftPageNumber = leftPageNumber + 2;
-			rightPageNumber = rightPageNumber + 2;
-			contentPane.repaint ();
-		});
+		turnPageLeftAction = new LoggingAction ("<", (ev) -> desiredPagesOnLeft++);
+		turnPageRightAction = new LoggingAction (">", (ev) -> desiredPagesOnLeft--);
 		
 		// Initialize the content pane
 		contentPane = new JPanel ()
@@ -144,25 +151,29 @@ public final class SpellBookNewUI extends MomClientFrameUI
 			protected final void paintComponent (final Graphics g)
 			{
 				super.paintComponent (g);
-				g.drawImage (cover, 0, (ANIM_VERTICAL_OFFSET + OLD_BACKGROUND_PADDING_TOP) * 2, cover.getWidth () * 2, cover.getHeight () * 2, null);
+				g.drawImage (cover, 0, BACKGROUND_PADDING_TOP * 2, cover.getWidth () * 2, cover.getHeight () * 2, null);
 				
-				// There's always 1 left page and 1 right page at the bottom
-				g.drawImage (pageLeft, FIRST_LEFT_PAGE, FIRST_PAGE_TOP, null);
-				g.drawImage (pageRight, FIRST_RIGHT_PAGE, FIRST_PAGE_TOP, null);
-				
-				// How many additional pages to draw on each side?
-				final int additionalLeftPages = leftPageNumber / 2;
-				final int additionalRightPages = FLIPPABLE_PAGE_COUNT - additionalLeftPages;
-				
-				for (int n = 1; n <= FLIPPABLE_PAGE_COUNT; n++)
+				for (int leftIndex = 0; leftIndex < pageState.size (); leftIndex++)
 				{
-					if (n <= additionalLeftPages)
-						g.drawImage ((n == additionalLeftPages) ? pageLeftCorner : pageLeft,
-							FIRST_LEFT_PAGE + (n * PAGE_SPACING_X), FIRST_PAGE_TOP - (n * PAGE_SPACING_Y), null);
-					
-					if (n <= additionalRightPages)
-						g.drawImage ((n == additionalRightPages) ? pageRightCorner : pageRight,
-							FIRST_RIGHT_PAGE - (n * PAGE_SPACING_X), FIRST_PAGE_TOP - (n * PAGE_SPACING_Y), null);
+					// Draw left pages (those with pageState <= 5) in order, since page 0 is at the bottom of the left pile
+					final int leftPageState = pageState.get (leftIndex);
+					if (leftPageState < pageLeftFrames.size ())
+					{
+						final BufferedImage image = pageLeftFrames.get (leftPageState);
+						g.drawImage (image, FIRST_LEFT_PAGE + (leftIndex * PAGE_SPACING_X), FIRST_PAGE_BOTTOM - image.getHeight () - (leftIndex * PAGE_SPACING_Y), null);
+					}
+				}
+				
+				for (int fromRight = 0; fromRight < pageState.size (); fromRight++)
+				{
+					// Draw right pages (those with pageState >= 6) in reverse order, since page (max) is at the bottom of the right pile
+					final int rightIndex = pageState.size () - 1 - fromRight;
+					final int rightPageState = pageState.get (rightIndex);
+					if (rightPageState >= pageLeftFrames.size ())
+					{
+						final BufferedImage image = pageRightFrames.get (pageStateCount - 1 - rightPageState);
+						g.drawImage (image, FIRST_RIGHT_PAGE - (fromRight * PAGE_SPACING_X), FIRST_PAGE_BOTTOM - image.getHeight () - (fromRight * PAGE_SPACING_Y), null);
+					}
 				}
 
 				/* drawCurve (g, 77, 47, 106, 243);
@@ -194,6 +205,78 @@ public final class SpellBookNewUI extends MomClientFrameUI
 		getFrame ().setResizable (false);
 		getFrame ().setUndecorated (true);
 		setWindowID (WindowID.SPELL_BOOK);
+		
+		// Update the animation each tick
+		final Timer pageTurnTimer = new Timer (1000 / PAGE_FLIP_ANIMATION_SPEED, (ev) ->
+		{
+			boolean updated = false;
+			
+			// Go through page in turn
+			// I don't think this is perfect - whether we need to process the pages left-first or right-first depends which way they are moving
+			// so the animation frames will be 1 out in one direction compared to the other... but you probably can't notice this when its moving fast
+			for (int leftIndex = 0; leftIndex < pageState.size (); leftIndex++)
+			{
+				final int leftPageState = pageState.get (leftIndex);
+				final int leftPageDesiredState = (leftIndex < desiredPagesOnLeft) ? 0 : (pageStateCount - 1);
+				if (leftPageState != leftPageDesiredState)
+				{
+					// Increase or decrease it?
+					final int leftPageNewState = (leftPageDesiredState > leftPageState) ? (leftPageState + 1) : (leftPageState - 1);
+					if (newStateWouldBeValid (leftIndex, leftPageNewState))
+					{
+						pageState.set (leftIndex, leftPageNewState);
+						updated = true;
+					}
+				}
+			}
+			
+			if (updated)
+				contentPane.repaint ();
+		});
+		pageTurnTimer.start ();
+	}
+	
+	/**
+	 * @param index Index into pageState list that we want to update
+	 * @param value Value we want to update it to
+	 * @return Whether the new list would be valid, i.e. is the updated value too close to either adjacent value
+	 */
+	private final boolean newStateWouldBeValid (final int index, final int value)
+	{
+		boolean valid = true;
+		
+		if (index > 0)
+		{
+			final int prevValue = pageState.get (index - 1);
+			if ((prevValue != 0) && (prevValue != pageStateCount - 1) &&
+				(Math.abs (value - prevValue) < ANIMATION_MINIMUM_SEPARATION))
+				
+				valid = false;
+		}
+
+		if ((index + 1) < pageState.size ())
+		{
+			final int nextValue = pageState.get (index + 1);
+			if ((nextValue != 0) && (nextValue != pageStateCount - 1) &&
+				(Math.abs (value - nextValue) < ANIMATION_MINIMUM_SEPARATION))
+				
+				valid = false;
+		}
+		
+		// Also the states have to be in ascending order - so if we block a page moving, the one behind it can't start moving
+		int check = 0;
+		while ((valid) && (check + 1 < pageState.size ()))
+		{
+			final int thisValue = (check == index) ? value : pageState.get (check);
+			final int nextValue = (check + 1 == index) ? value : pageState.get (check + 1);
+			
+			if (thisValue > nextValue)
+				valid = false;
+			else			
+				check++;
+		}
+		
+		return valid;
 	}
 	
 	/**
